e90412b4d42294fbc7b055e97cd7220d925ac2d2
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2023-01-09 15:19:17 +0100

    created models migrationand controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/jobs', function () {
+    return Job::all();
+});
+
+Route::post('/jobs', function () {
+    return Job::create([
+        'name' => 'Job One',
+        'description' => 'description',
+        'location' => 'location',
+
+    ]);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
